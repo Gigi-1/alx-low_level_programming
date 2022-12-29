a0ddc7f447e9fbc71dabe72e1e4a83584c3393e8
giseleapedo <giseleapedo@icloud.com>
2022-12-29 07:17:22 -0800

    task 9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,7 +2,7 @@
 
 /**
  * times_table - prints the 9 times table, starting with 0
- * */
+ */
 
 void times_table(void)
 {
@@ -17,7 +17,6 @@ void times_table(void)
 			{
 				_putchar(k + '0');
 			}
-			
 			if (k < 10 && j != 0)
 			{
 				_putchar(',');
